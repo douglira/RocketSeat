55a2e4c7df6a6191cb0354bb73d5c1ed588617b7
Douglas Lira <dodo_1828@hotmail.com>
2018-04-23 16:43:55 -0300

    backup

--- testando-react-native/src/App.js
+++ testando-react-native/src/App.js
@@ -1,8 +1,46 @@
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Text style={{
+        fontSize: 42, 
+        height: 150,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        backgroundColor: 'darkblue', 
+        color: '#FFF'
+      }}>Vocalize</Text>
+    )
+  }
+}
+
+const AuthStack = TabNavigator(
+  { 
+    Posts: { screen: Posts },
+    Profile: { screen: Profile },
+  },
+  {
+    tabBarOptions: {
+      indicatorStyle:{
+        backgroundColor: 'purple'
+      },
+      style: {
+        backgroundColor: 'darkblue'
+      }
+    }
+  }
+)
 
 const App = StackNavigator({
-  Posts: { screen: Posts },
-  Profile: { screen: Profile }
+  AuthStack: {
+    screen: AuthStack, 
+    navigationOptions: { 
+      header: (<Header/>)
+    }
+ }
 })
 
 import Posts from './screens/posts';

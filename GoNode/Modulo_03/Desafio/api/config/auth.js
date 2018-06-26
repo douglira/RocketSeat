8e2d35b51744dcc9efa811cff13e5448dc0c3013
Douglas Lira <douglas.vclira@gmail.com>
2018-06-25 23:24:43 -0300

    Facerocket moved

--- GoNode/Modulo_03/Desafio/api/config/auth.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  secret: 'dff3fea5f626f4f7fea0770b47f4a29b',
-  expireTokenTime: 86400,
-};
--- GoNode/Modulo_03/Desafio/frontend/src/pages/Post/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  display: flex;
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  padding: 10px 20px;
-`;

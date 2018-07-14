6904f9019c9d9818f5966c962bfe8bcf6f6e4967
Douglas Lira <douglas.vclira@gmail.com>
2018-07-14 20:25:08 -0300

    airbnb clone - api with adonisjs framework (users and properties routes)

---
+++ airbnb-clone/adonisjs-api/start/routes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Route = use('Route');
+
+Route.post('/users', 'UserController.create');
+
+Route.post('/sessions', 'SessionController.create');
+
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth');

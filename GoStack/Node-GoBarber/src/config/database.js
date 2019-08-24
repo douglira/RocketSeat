cd691080f850635d09e5b7dd0668e0011f572f02
Douglas Lira <douglas.vclira@gmail.com>
2019-08-23 21:58:27 -0300

    nodejs/gobarber: users migration

---
+++ GoStack/Node-GoBarber/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'douglas',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
